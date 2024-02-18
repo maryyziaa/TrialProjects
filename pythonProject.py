2ea934723a72c56739e824e507f33dc40d188573
maryyziaa <160417204+maryyziaa@users.noreply.github.com>
2024-02-18 22:11:29 +0530

    Add files via upload

---
+++ pythonProject.py
@@ -0,0 +1,43 @@
+import time
+
+str = "This is my first Python Project!" 
+word_count = len(str.split())
+
+def createbox():
+    print("/////"*30)
+    print()
+    print('Type the phrase provided below as fast as possible & with accuracy') 
+    print()
+
+while True:  
+    t0 = time.time()
+    createbox()
+    print(str,'\n')
+    inputText = str(input())
+    t1 = time.time()
+    lengthOfInput = len(inputText.split())
+    accuracy = len(set(inputText.split()) & set(str.split())) 
+    accuracy = (accuracy/word_count)
+    timeTaken = (t1 - t0)
+
+    wordsperminute = (lengthOfInput/timeTaken)*60
+    
+    print('Total words \t :' ,lengthOfInput)
+    print('Time used \t :',round(timeTaken,2),'seconds')
+    print('Your accuracy rate is\t :',round(accuracy,3)*100,'%') 
+    print('Speed is \t :' , round(wordsperminute,2),'words per minute') 
+    while True:
+        print("Do you want to retry? (Yes/No): ",end='')
+        ch=input().lower()
+        if ch == "yes":
+            break  
+        elif ch == "no":
+            print('Thank you for testing my project :)') 
+            time.sleep(1.5) 
+            break  
+        else:
+            print("Invalid choice. Please enter 'yes' or 'no'.")  
+            continue  
+
+    if ch == "no":
+        break
